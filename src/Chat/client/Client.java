dbe747e6f3a8be242eb806a38ddd6a71f2db55ab
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-04-28 15:52:13 +0300

    chat 12

---
+++ src/Chat/client/Client.java
@@ -0,0 +1,11 @@
+package Chat.client;
+
+import com.javarush.task.task30.task3008.Connection;
+
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected;
+
+    public class SocketThread extends Thread {
+    }
+}
