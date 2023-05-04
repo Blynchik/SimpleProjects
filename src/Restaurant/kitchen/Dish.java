a7da16d17c7b7747e71ed847438821099d14795a
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-05-04 18:23:00 +0300

    rest 1

---
+++ src/Restaurant/kitchen/Dish.java
@@ -0,0 +1,22 @@
+package Restaurant.kitchen;
+
+public enum Dish {
+    FISH,
+    STEAK,
+    SOUP,
+    JUICE,
+    WATER;
+
+    public static String allDishesToString() {
+        String result = "";
+
+        for (Dish dish : Dish.values()) {
+            if ("".equals(result)) {
+                result += dish.name();
+            } else {
+                result += ", " + dish.name();
+            }
+        }
+        return result;
+    }
+}
