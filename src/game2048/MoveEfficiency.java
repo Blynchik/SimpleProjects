5cd89c605a5d6276c72fee7e91a019f70c8d4efb
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-05-15 16:58:20 +0300

    14

---
+++ src/game2048/MoveEfficiency.java
@@ -0,0 +1,17 @@
+package game2048;
+
+class MoveEfficiency {
+    private int numberOfEmptyTiles;
+    private int score;
+    private Move move;
+
+    public MoveEfficiency(int numberOfEmptyTiles, int score, Move move) {
+        this.numberOfEmptyTiles = numberOfEmptyTiles;
+        this.score = score;
+        this.move = move;
+    }
+
+    public Move getMove() {
+        return move;
+    }
+}
