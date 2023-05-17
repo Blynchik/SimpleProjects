55d003e202dd13835264c679bb72f1f11a7d324c
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-05-17 16:39:38 +0300

    space 7

--- src/Space/Canvas.java
+++ src/Space/Canvas.java
@@ -1,4 +1,26 @@
 package Space;
 
 public class Canvas {
+
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new char[height][width];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
 }
