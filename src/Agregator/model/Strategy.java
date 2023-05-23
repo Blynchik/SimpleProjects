42192c6d4173802c5ddec3fe67998d4d0bf5021c
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-05-23 10:55:59 +0300

    agregator 5

--- src/Agregator/model/Strategy.java
+++ src/Agregator/model/Strategy.java
@@ -1,4 +1,10 @@
 package Agregator.model;
 
+import com.javarush.task.task28.task2810.vo.Vacancy;
+
+import java.util.List;
+
 public interface Strategy {
+
+    List<Vacancy> getVacancies(String searchString);
 }
