993a38ae55e341f767623cd065f33785ca44323c
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-05-24 08:22:22 +0300

    cash 3

---
+++ src/CashMachine/CurrencyManipulator.java
@@ -0,0 +1,18 @@
+package CashMachine;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CurrencyManipulator {
+    private String currencyCode;
+    private Map<Integer, Integer> denominations;
+
+    public CurrencyManipulator(String currencyCode) {
+        this.currencyCode = currencyCode;
+        this.denominations = new TreeMap<>();
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+}
