a311b0817c430119d41b2ccc017be858192e29fd
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-05-24 09:41:49 +0300

    sok 3

---
+++ src/Sokoban/view/Field.java
@@ -0,0 +1,16 @@
+package Sokoban.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Field extends JPanel {
+    View view;
+
+    public Field(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+    }
+}
